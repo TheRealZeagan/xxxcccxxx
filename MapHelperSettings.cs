81bacdb88a4cd4681d113e0e7c82880257c0e76d
TheRealZeagan <jonaspepsimax@hotmail.com>
2021-02-05 19:21:34 -0100

    Add files via upload

--- MapHelperSettings.cs
+++ MapHelperSettings.cs
@@ -21,9 +21,9 @@ namespace MapHelper
         public ToggleNode Enable { get; set; }
         public HotkeyNode MapHelperKey { get; set; }
 
-        public ToggleNode Physical Reflect { get; set; }
-        public ToggleNode Reduced Block { get; set; }
-        public ToggleNode No Regen { get; set; }
+        public ToggleNode PhysicalReflect { get; set; }
+        public ToggleNode ReducedBlock { get; set; }
+        public ToggleNode No egen { get; set; }
         //public RangeNode<float>NoRegenValue { get; set; } 
 
 
